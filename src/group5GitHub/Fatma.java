7477e73bc69c0eeb9f5616ec82d292b88861fe06
Fatma Guleryuz <fglryz.111@gmail.com>
2022-05-09 21:30:23 +0100

    Frequency of element

--- src/group5GitHub/Fatma.java
+++ src/group5GitHub/Fatma.java
@@ -3,5 +3,16 @@ package group5GitHub;
 public class Fatma {
     public static void main(String[] args) {
         System.out.println("We will be good in GitHub ");
+
+
     }
-}
+    public static int frequencyOfElement(char[] array, char element) {
+        int count = 0;
+        for (char each : array) {
+            if (each == element) {
+                count++;
+            }
+
+        }
+        return count;
+}}
