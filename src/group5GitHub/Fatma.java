27a6108d8f1837341fc1ed03b58b2361b00e14a7
hanifezynp85 <hanifebayergok@gmail.com>
2022-05-10 12:28:49 +0100

    written something

--- src/group5GitHub/Fatma.java
+++ src/group5GitHub/Fatma.java
@@ -3,5 +3,9 @@ package group5GitHub;
 public class Fatma {
     public static void main(String[] args) {
         System.out.println("We will be good in GitHub ");
+
+        System.out.println("can you see me changings");
     }
+
+
 }
